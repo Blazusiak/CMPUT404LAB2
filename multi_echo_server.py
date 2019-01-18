582a18e9c4603b61f5f901d91a477b9d3adc1420
Student <jblazusi@ualberta.ca>
2019-01-17 20:28:55 -0500

    multi server/client

---
+++ multi_echo_server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import socket
+import time
+from multiprocessing import Process
+
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+def main():
+    google_addr = None
+
+    addr_info = socket.getaddrinfo("www.google.com", 80)
+    for addr in addr_info:
+        (family, socktype, proto, canonname, sockaddr) = addr
+        if family == socket.AF_INET and socktype == socket.SOCK_STREAM:
+            google_addr = addr
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen(2)
+        while True:
+            conn, addr = s.accept()
+            p = Process(target=handle_echo, args=(addr, conn))
+            p.daemon = True
+            p.start()
+            print("Started process ", p)
+
+def handle_echo(addr, google_addr, conn):
+    print("Connected by", addr)
+
+    (family, socktype, proto, canonname, sockaddr) = google_addr
+    with socket.socket(family, socktype) as proxy_end:
+        proxy_end.connect(sockaddr)
+
+        send_full_data = b""
+        while True:
+            data = conn.recv(BUFFER_SIZE)
+            if not data: break
+            send_full_data += data
+
+        proxy_end.sendall(send_full_data)
+        proxy_end.shutdown(socket.SHUT_WR)
+
+        while True:
+            data = proxy_end.recv(BUFFER_SIZE)
+            if not data: break
+            conn.send(data)
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
