356565aa4c1489e9cd9445a52515be521eadeb2f
Student <jblazusi@ualberta.ca>
2019-01-17 20:39:56 -0500

    Updated Client/Server

--- proxy_client.py
+++ proxy_client.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+
 import socket
 
 HOST = "localhost"
 PORT = 8001
 BUFFER_SIZE = 1024
 
+# payload = """GET / HTTP/1.0
+# Host: {}
+
+# """.format(HOST)
+
+# payload = f"GET / HTTP/1.0\r\nHost: {HOST}\r\n\r\n"
 payload = "GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n"
 
 def get_request(addr):
@@ -21,17 +28,15 @@ def get_request(addr):
             if not data: break
             full_data += data
         print(full_data)
-
     except Exception as e:
         print(e)
-
     finally:
         s.close()
 
 def main():
     addr_info = socket.getaddrinfo(HOST, PORT)
     for addr in addr_info:
-        family, socktype, proto, canonname, sockaddr = addr
+        (family, socktype, proto, canonname, sockaddr) = addr
         if family == socket.AF_INET and socktype == socket.SOCK_STREAM:
             get_request(addr)
 
